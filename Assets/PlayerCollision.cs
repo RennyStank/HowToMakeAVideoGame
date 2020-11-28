507c9e9650a019f0883536b3dec950706f0a796f
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-11-28 23:35:28 +0800

    Initial commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement pm;
+    public Text t;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle") {
+            pm.enabled = false;
+            t.text = "You Lost";
+        }
+    }
+
+}
